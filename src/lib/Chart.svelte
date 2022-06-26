1368874e375aff11a043684cd1d7dd1dce2f0987
worawutbat <woravootbat@gmail.com>
2022-06-26 21:07:20 +0700

    🎉 set up new project with svelte

---
+++ src/lib/Chart.svelte
@@ -0,0 +1,45 @@
+
+    
+    <script>
+        let canvas;
+        
+        import Chart from "chart.js/auto/auto.js";
+        import { onMount } from 'svelte';
+
+        onMount(() => {
+
+        var ctx = canvas.getContext("2d");
+
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+        })
+        })
+        
+    </script>
+
+<canvas bind:this={canvas} width="400" height="400"></canvas>
+
